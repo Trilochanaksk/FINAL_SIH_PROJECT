039ee35dcbf1624e41052f05b7c4360464faf09b
harikadevi2006 <harikadevi349@gmail.com>
2025-09-23 02:04:34 +0000

    not at all good give some more good and professional

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -6,16 +6,17 @@ export const Logo = ({ className }: { className?: string }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth="1.5"
     strokeLinecap="round"
     strokeLinejoin="round"
     className={cn("size-6", className)}
   >
     <title>AyuLink Logo</title>
     <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
-    <path d="M12 2a10 10 0 0 0-3.465 19.34" />
-    <path d="M12 2a10 10 0 0 1 3.465 19.34" />
-    <path d="M2 12h20" />
-    <path d="M12 2v20" />
+    <path d="M12 8c-1.105 0-2 .895-2 2s.895 2 2 2 2-.895 2-2-.895-2-2-2z" />
+    <path d="M12 12c-2.21 0-4 1.79-4 4h8c0-2.21-1.79-4-4-4z" />
+    <path d="M9.5 12c0-1.38-1.12-2.5-2.5-2.5S4.5 10.62 4.5 12" />
+    <path d="M14.5 12c0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5" />
+    <path d="M12 16v2" />
   </svg>
 );
