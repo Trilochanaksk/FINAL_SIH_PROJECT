7df51caf62703543728bd95fd66693fc3a1d4508
harikadevi2006 <harikadevi349@gmail.com>
2025-09-24 11:05:40 +0000

    again minimize just like a nice chatbot icon

--- src/components/global-chatbot.tsx
+++ src/components/global-chatbot.tsx
@@ -15,14 +15,14 @@ export default function GlobalChatbot() {
             <Button
                 onClick={openChatbot}
                 className={cn(
-                    'fixed bottom-6 right-6 z-40 h-20 w-20 rounded-full p-2 transition-transform hover:scale-110 bg-transparent'
+                    'fixed bottom-6 right-6 z-40 h-16 w-16 rounded-full p-2 transition-transform hover:scale-110 bg-transparent'
                 )}
             >
               <Image
                 src="https://i.pinimg.com/736x/3f/7f/51/3f7f51eeaa56b4c8a80c0c64bc8fa103.jpg"
                 alt="Chatbot Icon"
-                width={80}
-                height={80}
+                width={64}
+                height={64}
                 className="rounded-full"
               />
             </Button>
